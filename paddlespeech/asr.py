38365ab7fba44db07ab536563995a7144d56e18e
zyh <745644582@qq.com>
2023-07-22 09:38:54 +0800

    中英文混合识别与合成

--- paddlespeech/asr.py
+++ paddlespeech/asr.py
@@ -1,5 +1,14 @@
 from paddlespeech.cli.asr.infer import ASRExecutor
+from paddlespeech.cli.text import TextExecutor
 
 asr = ASRExecutor()
-result = asr(audio_file="银耳浆制作.wav")
+result = asr(
+    model="conformer_talcs",
+    lang="zh_en",
+    codeswitch=True,
+    audio_file="ch_zh_mix.wav")
+print(result)
+
+text_executor = TextExecutor()
+result = text_executor(text=result)
 print(result)
