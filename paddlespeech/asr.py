d3ac9ae8a760b0408d6a1a66e686bea1c3603ade
zyh <745644582@qq.com>
2023-07-30 15:18:02 +0800

    去掉二进制文件

--- paddlespeech/asr.py
+++ paddlespeech/asr.py
@@ -1,14 +1,13 @@
 from paddlespeech.cli.asr.infer import ASRExecutor
 from paddlespeech.cli.text import TextExecutor
 
-asr = ASRExecutor()
-result = asr(
-    model="conformer_talcs",
-    lang="zh_en",
-    codeswitch=True,
-    audio_file="ch_zh_mix.wav")
-print(result)
-
+asr_executor = ASRExecutor()
 text_executor = TextExecutor()
-result = text_executor(text=result)
-print(result)
+
+for i in range(22):
+    file = f"./voice/3/output_{i}.wav"
+    if i < 10:
+        file = f"./voice/3/output_0{i}.wav"
+    text = asr_executor(audio_file=file, lang="zh")
+    result = text_executor(text=text)
+    print(result)
